c3dcfd96799d7f02732f04b40c426a68df00eb6a
Vinutha015 <101888843+Vinutha015@users.noreply.github.com>
2022-04-03 23:10:43 +0530

    Add files via upload

---
+++ 4_Test plan and output/OUTPUT.C
@@ -0,0 +1,38 @@
+ Movie Ticket booking 
+
+
+Press <1> Insert Movie
+Press <2> View All Movie
+Press <3> Find Movie 
+Press <4> Book Ticket
+Press <5> View All Transactions
+Press <0> Exit 
+Enter your Choice ::
+1
+Enter movie code :- 
+R
+Enter  name :- 
+RRR
+Enter Release Date:- 
+25
+
+Enter movie language:-
+Telugu
+
+Enter Ticket Price:- 
+200
+Record insert Successful
+
+
+	 Movie Ticket booking 
+
+
+Press <1> Insert Movie
+Press <2> View All Movie
+Press <3> Find Movie 
+Press <4> Book Ticket
+Press <5> View All Transactions
+Press <0> Exit 
+Enter your Choice ::
+
+Session Killed due to Timeout.
